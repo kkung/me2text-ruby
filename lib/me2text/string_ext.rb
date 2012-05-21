009b3c0a6b929a5467e2c6d7796ef0a47122c115
codian <codian@gmail.com>
2012-05-21 21:48:58 +0900

    * String 클래스 확장

---
+++ lib/me2text/string_ext.rb
@@ -0,0 +1,26 @@
+require 'me2text'
+
+module Me2Text
+  module StringExtension
+    def self.included(base)
+      base.extend ClassMethods
+      base.send(:include, InstanceMethods)
+    end
+  
+    module InstanceMethods 
+      def me2text(format = :html, options = {})
+        Me2Text.me2text(self, format, options)
+      end
+    end
+  
+    module ClassMethods 
+      def me2text(text, format = :html, options = {})
+        Me2Text.me2text(text, format, options)
+      end
+    end
+  end
+end
+
+class String
+  include Me2Text::StringExtension
+end
